92ac854ed47b991e0fd370436ed00645d2b8ef17
nariya <nariya@shota-2.local>
2020-10-28 19:24:13 +0900

    imaje導入

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App({ Imajeitem }) {
+  const [imajeitem, setImajeitem] = useState("https://placehold.jp/200x200.png");
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +12,19 @@ function App() {
         </div>
         <textarea></textarea>
       </header>
+      <div class="imaje-foto">
+        <img src={imajeitem} alt={"風景画像"} />
+        <img src={imajeitem} alt={"風景画像"} />
+        <img src={imajeitem} alt={"風景画像"} />
+        <img src={imajeitem} alt={"風景画像"} />
+        <div class="imaje-item"></div>
+        <div class="imaje-item"></div>
+        <div class="imaje-item"></div>
+        <div class="imaje-item"></div>
+      </div>
+
     </div>
+
   );
 }
 

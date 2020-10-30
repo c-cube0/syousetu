699cc58cc4758ac1d28fbc46e55831b12d708903
Shota Miyahira <2525.natuta@gmail.com>
2020-10-30 18:19:22 +0900

    不要な文字が入っていたので削除

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />, document.getElementById("root")
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
